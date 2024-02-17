a7845733d00ff48eb97c40218a0e7c32afe9b13c
phuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2024-02-17 18:02:25 +0700

    jj

--- Prototype.cpp
+++ Prototype.cpp
@@ -12,19 +12,56 @@ int randomcards(void){
 
 int main() {
     srand(time(0));
-    string name;
-    int input;
-    cout << "หมอปลาทอง: คุณชื่ออะไร?" << endl;
+    string name,ans,input;
+    cout << "Prophet: What is your name?" << endl;
     cout << "???: ";
-    cin >> name;
-    cout << endl;
-    cout << "หมอปลาทอง: โปรดเลือกการ์ดเพื่อดูดวงชะตาของคุณ" << endl;
-    for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
-        cout << "[" << i + 1 << "]" << " ";}
-    cout << endl;
-    cout << name << ": ";
-    cin >> input;
-    cout << "หมอปลาทอง: คุณได้ " << cards[randomcards()] << endl;
+    getline(cin,name);
+    cout << "Prophet: What would you like to consult about?" << endl;
+    cout << "[1]Love [2]Study [3]Health [4]Money [5]dooduang" << endl;
+    cin >> ans;
+    cin.ignore();
+    if(ans == "5"){
+        cout << "Prophet: Please choose your cards" << endl;
+        for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
+            cout << "[" << i + 1 << "]" << " ";}
+        cout << endl;
+        cout << name << ": ";
+        cin >> input;
+        cout << "Prophet: You got " << cards[randomcards()] << endl;
+    }else if (ans=="4"){
+        cout << "Prophet: Please choose your cards" << endl;
+        for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
+            cout << "[" << i + 1 << "]" << " ";}
+        cout << endl;
+        cout << name << ": ";
+        cin >> input;
+        cout << "Prophet: You got " << cards[randomcards()] << endl;
+    }else if (ans=="3"){
+        cout << "Prophet: Please choose your cards" << endl;
+        for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
+            cout << "[" << i + 1 << "]" << " ";}
+        cout << endl;
+        cout << name << ": ";
+        cin >> input;
+        cout << "Prophet: You got " << cards[randomcards()] << endl;
+    }else if (ans=="2"){
+        cout << "Prophet: Please choose your cards" << endl;
+        for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
+            cout << "[" << i + 1 << "]" << " ";}
+        cout << endl;
+        cout << name << ": ";
+        cin >> input;
+        cout << "Prophet: You got " << cards[randomcards()] << endl;
+    }else if (ans=="1"){
+        cout << "Prophet: Please choose your cards" << endl;
+        for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
+            cout << "[" << i + 1 << "]" << " ";}
+        cout << endl;
+        cout << name << ": ";
+        cin >> input;
+        cout << "Prophet: You got " << cards[randomcards()] << endl;
+    }
+    
 
     return 0;
 }
