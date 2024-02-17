8e49541c04e9da34d048964ee975cf7baa2b3775
phuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2024-02-17 16:41:20 +0700

    v2

--- Prototype.cpp
+++ Prototype.cpp
@@ -12,12 +12,17 @@ int randomcards(void){
 
 int main() {
     srand(time(0));
+    string name;
     int input;
+    cout << "Prophet: What is your name?" << endl;
+    cout << "???: ";
+    cin >> name;
+    cout << endl;
     cout << "Prophet: Please choose your cards" << endl;
     for (int i = 0; i < sizeof(cards) / sizeof(cards[0]); i++){ 
         cout << "[" << i + 1 << "]" << " ";}
     cout << endl;
-    cout << "user: ";
+    cout << name << ": ";
     cin >> input;
     cout << "Prophet: You got " << cards[randomcards()] << endl;
 
